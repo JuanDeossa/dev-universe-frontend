a1b044ac65db4990d4612f0d8be980fd80cbcbea
ricdeo718 <juandeossa.nc@gmail.com>
2024-08-28 07:26:43 -0500

    test

--- src/api/services/auth/loginService.ts
+++ src/api/services/auth/loginService.ts
@@ -22,6 +22,32 @@ export const loginService = async ({
   console.log(paths.base);
 
   try {
+    await fetch("https://backend-release.up.railway.app/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "test@example.com",
+        password: "password123",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("data 1 :", data));
+
+    await fetch(`${paths.base}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "test@example.com",
+        password: "password123",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("data 2 :", data));
+
     const { data: resData } = await publicAxios.post(
       `auth/login`,
       {
