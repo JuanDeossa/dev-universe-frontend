a75422e54aad3b733a43eb1fcd5b2bc7dbb375c7
ricdeo718 <juandeossa.nc@gmail.com>
2024-08-28 07:43:27 -0500

    test

--- src/api/services/auth/loginService.ts
+++ src/api/services/auth/loginService.ts
@@ -35,7 +35,7 @@ export const loginService = async ({
       .then((response) => response.json())
       .then((data) => console.log("data 1 :", data));
 
-    await fetch(`${paths.base}/auth/login`, {
+    await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
